e34cdf979219acaaea16e0e42e575f849d8cee13
VS237 <victorinsonhofo@gmail.com>
2026-01-04 08:03:47 +0100

    ready for production

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['momshop-7.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
